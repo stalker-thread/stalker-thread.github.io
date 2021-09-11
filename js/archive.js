83c2006d0d3f8f7e4632501e0bf0e0bcf8829191
Vanya Archivist <shepard.jon11@yandex.ru>
2021-09-12 00:20:33 +0600

    Rebuild redirect

--- js/archive.js
+++ js/archive.js
@@ -3,19 +3,27 @@ document.addEventListener("DOMContentLoaded", function(){
   var curLink = table.rows[table.rows.length - 1].cells[3].children[0].href;
   var prevLink = table.rows[table.rows.length - 2].cells[3].children[0].href;
 
-  switch (window.location.hash) {
-    case "current":
-      window.location.replace(curLink);
-      break;
-    case "previous":
-      window.location.replace(prevLink);
-      break;
-    default:
-      break;
-  }
+  var redirect = function () {
+    console.log(window.location.hash);
+    switch (window.location.hash) {
+      case "#current":
+        console.log('current');
+        window.location.replace(curLink);
+        break;
+      case "#previous":
+        console.log('previous');
+        window.location.replace(prevLink);
+        break;
+      default:
+        break;
+    }
+  };
+
+  window.addEventListener("hashchange", redirect);
+  redirect();
 
   var bodies = table.tBodies;
-  var yearLinks = document.getElementsByClassName('year-link');
+  var yearLinks = document.getElementsByClassName("year-link");
   for (var i = 0; i < bodies.length; i++) {
     yearLinks[i].text = yearLinks[i].text + " (" + bodies[i].rows.length + ")";
   }
