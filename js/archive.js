30848e1f208c991d1e2006c4f060e048ddd4ae87
Vanya Archivist <shepard.jon11@yandex.ru>
2021-09-12 00:05:13 +0600

    Tag fixes, add support for redirect to latest arhivach threads

--- js/archive.js
+++ js/archive.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
   var table = document.getElementsByTagName('table')[0];
-  var lastTr = table.rows[table.rows.length - 1];
-  var link = lastTr.cells[3].children[0].href;
+  var curLink = table.rows[table.rows.length - 1].cells[3].children[0].href;
+  var prevLink = table.rows[table.rows.length - 2].cells[3].children[0].href;
+
+  switch (window.location.hash) {
+    case "current":
+      window.location.replace(curLink);
+      break;
+    case "previous":
+      window.location.replace(prevLink);
+      break;
+    default:
+      break;
+  }
 
   var latestLink = document.getElementById('latest_link');
   latestLink.href = link;
